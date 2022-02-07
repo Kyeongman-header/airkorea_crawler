3f9c08adce8f7d10f25e6f5a916095133f962398
Kyeongman <49219336+Kyeongman-header@users.noreply.github.com>
2022-02-08 00:55:00 +0900

    Update views.py

--- crawler/app/views.py
+++ crawler/app/views.py
@@ -52,7 +52,7 @@ def find_airkorea(gps):
     airkorea_datas=res.json()['response']['body']['items'][0]
     
     
-    return location, float(res.json()['response']['body']['items'][0]['so2Value']) if airkorea_datas['so2Flag']==None else 0, float(res.json()['response']['body']['items'][0]['coValue']) if airkorea_datas['coFlag']==None else 0, float(res.json()['response']['body']['items'][0]['o3Value']) if airkorea_datas['o3Flag']==None else 0,float(res.json()['response']['body']['items'][0]['no2Value']) if airkorea_datas['no2Flag']==None else 0,float(res.json()['response']['body']['items'][0]['pm25Value']) if airkorea_datas['pm25Flag']==None else 0, float(res.json()['response']['body']['items'][0]['khaiValue']) if (type(airkorea_datas['khaiValue']) is int) or (type(airkorea_datas['khaiValue']) is float) else 0
+    return location, float(res.json()['response']['body']['items'][0]['so2Value']) if airkorea_datas['so2Flag']==None else 0, float(res.json()['response']['body']['items'][0]['coValue']) if airkorea_datas['coFlag']==None else 0, float(res.json()['response']['body']['items'][0]['o3Value']) if airkorea_datas['o3Flag']==None else 0,float(res.json()['response']['body']['items'][0]['no2Value']) if airkorea_datas['no2Flag']==None else 0,float(res.json()['response']['body']['items'][0]['pm25Value']) if airkorea_datas['pm25Flag']==None else 0, float(res.json()['response']['body']['items'][0]['khaiValue']) if (airkorea_datas['khaiValue'] != '-') else 0
 
 
 
@@ -63,7 +63,18 @@ def gps(request):
     Y=request.GET.get('Y',None)
     point='SRID=4326;POINT (' + X + ' ' + Y + ')'
     gps={'gps':point}
-    location,so2,co,o3,no2,pm2_5,khai=find_airkorea(point)
+    try{
+        location,so2,co,o3,no2,pm2_5,khai=find_airkorea(point)
+    }
+    catch(e){
+        location="에어 코리아 unknown 오류"
+        so2=0
+        co=0
+        o3=0
+        no2=0
+        pm2_5=0
+        khai=0
+    }
 
     d={'gps':point,'location':location}
 
@@ -76,7 +87,7 @@ def gps(request):
         serializer1.save()
             # 이 저장 기능은 로그 기능을 위한 것으로, 그것도 어차피 개발 단계의 디버깅을 위한 것이다.
             #자체적으로 기간이 오래된 데이터는 삭제해주는 기능이 필요하다.
-
+        
         if (so2 != None) and (co != None) and (o3 != None) and (no2 != None) and (pm2_5 != None) and (khai != None) :
             d={ "airkorea" : {"P.M 2.5" : pm2_5, "CO" : co, "SO2" : so2, "O3" : o3, "NO2" : no2, "khai" : khai }}
             serializer2=AirKoreaSerializer(data=d)
